856d75a6fadcf725da1e6f106cdaba9004c1a798
David <David@192.168.20.117>
2015-11-05 15:47:38 +0100

    unused imports gone

--- AxisClient/src/ClientGui.java
+++ AxisClient/src/ClientGui.java
@@ -1,5 +1,7 @@
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
 import java.awt.EventQueue;
 import java.awt.Font;
 import java.awt.GridLayout;
@@ -16,17 +18,11 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JTextField;
-import javax.swing.SwingUtilities;
-import javax.swing.border.EmptyBorder;
 import javax.swing.JRadioButton;
-
-import org.omg.CORBA.FREE_MEM;
-import java.awt.Component;
-import java.awt.Dimension;
-import java.awt.FlowLayout;
-import javax.swing.SwingConstants;
 import javax.swing.JScrollPane;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.border.EmptyBorder;
 
 public class ClientGui extends JFrame {
 
